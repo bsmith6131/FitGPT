d413f5e8c1b06dad80ca68a80049ad307c59b850
Brian Smith <bsmith6130@icloud.com>
2025-07-13 13:15:13 -0400

    Initial Commit

---
+++ FitGPT/FitGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitGPTApp.swift
+//  FitGPT
+//
+//  Created by Brian Smith on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct FitGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitGPTTests/FitGPTTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitGPTTests.swift
+//  FitGPTTests
+//
+//  Created by Brian Smith on 7/13/25.
+//
+
+import Testing
+@testable import FitGPT
+
+struct FitGPTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
